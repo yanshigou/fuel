a20892391fbecd8190587839095592e5ccae04c3
cmx_company <569578851@qq.com>
2019-02-22 17:54:39 +0800

    .gitignore单元测试

--- autoTest.py
+++ autoTest.py
@@ -1,31 +1,30 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
 from django.test import Client
-from usermanager.models import User
+from fuelcalculation.models import UserProfile
 
 
-# class AdminLoginTest(TestCase):
-#     def setUp(self):
-#         User.objects.create_superuser(username='1', password='11111')
-#
-#     # url测试
-#     def test_login_url(self):
-#         c = Client()
-#         response = c.post('/usermanager/login', {"username": "1", "password": "11111"})
-#         self.assertEqual(response.status_code, 200)
-
-
-class Demo(TestCase):
+class AdminLoginTest(TestCase):
     def setUp(self):
-        print('setUp')
+        UserProfile.objects.create_user(username='222', password='11111')
 
-    def tearDown(self):
-        print('tearDown')
+    def test_login_url(self):
+        c = Client()
+        response = c.post('/fuelcal/login/', {"username": "222", "password": "11111"})
+        self.assertEqual(response.status_code, 200)
 
-    def test_demo(self):
-        print('test_demo')
 
-    def test_demo_2(self):
-        print('test_demo2')
+# class Demo(TestCase):
+#     def setUp(self):
+#         print('setUp')
+#
+#     def tearDown(self):
+#         print('tearDown')
+#
+#     def test_demo(self):
+#         print('test_demo')
+#
+#     def test_demo_2(self):
+#         print('test_demo2')
 
 
