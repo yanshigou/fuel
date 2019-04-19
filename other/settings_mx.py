7e36fe023824e754a4d180a6d8d2baae6f9c74e6
cmx_company <569578851@qq.com>
2019-04-19 10:54:44 +0800

    更换数据库

--- other/settings_mx.py
+++ other/settings_mx.py
@@ -79,32 +79,32 @@ WSGI_APPLICATION = 'fuel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'fuel',
-        'USER': 'dzt',
-        'PASSWORD': 'dzt123',
-        'HOST': '47.106.174.128',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
-    }
-}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'fuel',
-#         'USER': 'root',
-#         'PASSWORD': 'Rinima991022',
-#         'HOST': 'cq-cdb-f86d75dj.sql.tencentcdb.com',
-#         'PORT': '63999',
+#         'USER': 'dzt',
+#         'PASSWORD': 'dzt123',
+#         'HOST': '47.106.174.128',
+#         'PORT': '3306',
 #         'OPTIONS': {
 #             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
 #         }
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fuel',
+        'USER': 'root',
+        'PASSWORD': 'Rinima991022',
+        'HOST': 'cq-cdb-f86d75dj.sql.tencentcdb.com',
+        'PORT': '63999',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    }
+}
 
 
 # Password validation
