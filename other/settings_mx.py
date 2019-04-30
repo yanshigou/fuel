41b756f3b5b49ed12ffb4dfe7414cb794b484d11
cmx_company <569578851@qq.com>
2019-04-30 20:21:02 +0800

    DEBUG

--- other/settings_mx.py
+++ other/settings_mx.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 SECRET_KEY = '%n5*pngk^ggpcpbsljh(c2z(una_)62(bfof8+k#&a-ixle(x2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['47.106.174.128', 'www.dogebug.online']
 
